5859633c3d08988fb1aa3ea5ae87c2db16484a3b
Youssouf <youssouf.brahimazib@vinci-autoroutes.com>
2022-11-17 11:29:15 +0100

    Load an image using a plugin gatsby-plugin-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import Layout from "../components/layout";
 
@@ -5,6 +6,10 @@ const HomePage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Je suis la page Home.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
+      />
     </Layout>
   );
 };
